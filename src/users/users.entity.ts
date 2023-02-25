cde51d181d60e54d1f5d8a334e9c70b2e3621b24
Barney <holyja@naver.com>
2023-02-25 22:08:51 +0900

    feat: Add User entity & Add service

---
+++ src/users/users.entity.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  kakaoId: string;
+
+  @Column()
+  image: string;
+
+  @Column({ default: true })
+  isValid : boolean;
+
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  createdAt: string;
+
+  @Column()
+  updatedAt: string;
+}
