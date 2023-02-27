8a105b7d8580a7eaae333705a687ef62c55fe427
Geon gyu Park <geongyupark@Geonui-MacBookAir-2.local>
2023-02-27 20:11:35 +0900

    feat: Add swagger feature

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,23 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { Users } from './users.entity';
 import { UsersService } from './users.service';
+import { ApiTags, ApiOperation, ApiResponse, ApiCreatedResponse } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('User')
 export class UsersController {
     constructor(private readonly usersService: UsersService){}
     
     @Get()
+    @ApiOperation({summary: '전체 유저 조회 API', description: '전체 유저를 조회합니다.'})
+    @ApiResponse({description: '전체 유저의 정보를 Array 형태로 반환합니다.', type: [Users], status: 200})
     findAll(){
         return this.usersService.findAll()
     }
 
     @Post()
+    @ApiOperation({summary: '신규 유저 생성 API', description: '신규 유저를 생성합니다.'})
+    @ApiCreatedResponse({description: '신규 생성한 유저의 정보를 반환합니다.', type: Users})
     create(@Body() user: Users){
         this.usersService.create(user);
 
--- src/users/users.entity.ts
+++ src/users/users.entity.ts
@@ -1,37 +1,49 @@
 import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { ApiProperty } from '@nestjs/swagger' 
 
 @Entity()
 export class Users {
+  
+  @ApiProperty()
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty()
   @Column({length: 200})
   email: string;
 
+  @ApiProperty()
   @Column({length: 100})
   kakaoId: string;
 
+  @ApiProperty()
   @Column({length: 100})
   image: string;
 
+  @ApiProperty()
   @Column({ default: true })
   isValid : boolean;
 
+  @ApiProperty()
   @Column({length: 50})
   firstName: string;
 
+  @ApiProperty()
   @Column({length: 50})
   lastName: string;
 
+  @ApiProperty()
   @Column({length: 20})
   phone: string;
 
+  @ApiProperty()
   @Column({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP'
   })
   createdAt: Date
 
+  @ApiProperty()
   @Column({
     type: 'timestamp',
     default: () => 'CURRENT_TIMESTAMP',
